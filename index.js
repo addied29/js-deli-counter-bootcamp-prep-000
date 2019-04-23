4ce4d8f2c0ac534724900180b4fefae9d61377f8
Addie Dean <addiedean@gmail.com>
2019-04-23 16:10:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 var katzDeliLine = [ ]
+var i=0
 
-function takeANumber(katzDeliLine,name){
-  katzDeliLine.push(`${name}`)
-  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+function takeANumber(katzDeliLine){
+  i++
+  katzDeliLine.push(i)//
+  return(`You are number ${katzDeliLine.length}`)
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length===0)
+  if (katzDeliLine.length===0){
   return ('There is nobody waiting to be served!')
+  }
   else {
   return (`Currently serving ${katzDeliLine.shift()}.`) 
 }
 }
 var katzDeliLine = [ ]
 function currentLine(katzDeliLine){
-  
+ var nameNumber=[] 
   if (katzDeliLine.length === 0)
   return ('The line is currently empty.')
   else{
-    for (i=0; i<katzDeliLine.length; i++) {
-      var nameNumber[i] = ""+ ${i}"."${katzDeliLine[i]}
+    for (let i=0; i<katzDeliLine.length; i++) {
+      nameNumber[i] = ` ${i+1}. ${katzDeliLine[i]}`
     }
-  return (`The line is currently: ${nameNumber}` )
+  return (`The line is currently:${nameNumber}` )
   }
 }
